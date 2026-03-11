89a5e310f3daa6d43c41ff05949d099558e8e2f1
wakhataephraim09 <grandyjones09@gmail.com>
2026-03-11 17:31:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Task Manager Loaded")
+const addBtn = document.getElementById("addTaskBtn")
+
+addBtn.addEventListener("click", function(){
+
+const input = document.getElementById("taskInput")
+
+const taskText = input.value
+
+if(taskText === "") return
+
+const li = document.createElement("li")
+
+li.textContent = taskText
+
+const deleteBtn = document.createElement("button")
+
+deleteBtn.textContent = "Delete"
+
+deleteBtn.onclick = function(){
+li.remove()
+}
+
+li.appendChild(deleteBtn)
+
+document.getElementById("taskList").appendChild(li)
+
+input.value = ""
+
+})
